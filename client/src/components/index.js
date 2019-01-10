db24bee3c56203df5870471bf358d46369fe9440
Greg Hamel <hamegreg@gmail.com>
2019-01-10 14:48:39 -0500

    Remove client and make a new project for it

--- client/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Content } from "./content.react";
-export { default as Header } from "./header.react";
-export { default as Metars } from "./metars.react";
-export { default as Notams } from "./notams.react";
-export { default as Query } from "./query.react";
-export { default as Rvr } from "./rvr.react";
-export { default as StationNav } from "./stationNav.react";
-export { default as Tafs } from "./tafs.react";
