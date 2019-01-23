25ddedc565d212f66d10b8cf5297ee25c1c3d2ae
Greg Hamel <hamegreg@gmail.com>
2019-01-23 17:52:01 -0500

    test(client): update tests for old components

--- client/src/components/__tests__/header.test.js
+++ client/src/components/__tests__/header.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render, cleanup } from 'react-testing-library'
 import ReactDOM from 'react-dom';
 import Header from '../header.react';
+import { BrowserRouter } from 'react-router-dom';
 import 'jest-dom/extend-expect';
 afterEach(cleanup)
 
 it('header renders properly', () => {
-  const { container } = render(<Header />)
+  const { container } = render(<BrowserRouter><Header /></BrowserRouter>)
   const header = container.firstChild
   expect(header).toMatchSnapshot()
 });
--- client/src/components/__tests__/searchbox.test.js
+++ client/src/components/__tests__/searchbox.test.js
@@ -3,10 +3,11 @@ import { render, cleanup } from 'react-testing-library'
 import ReactDOM from 'react-dom';
 import Searchbox from '../searchbox.react';
 import 'jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
 afterEach(cleanup)
 
 it('searchbox renders properly', () => {
-  const { container } = render(<Searchbox />)
+  const { container } = render(<BrowserRouter><Searchbox /></BrowserRouter>)
   const searchbox = container.firstChild
   expect(searchbox).toMatchSnapshot()
 });
