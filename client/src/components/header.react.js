71dc633a90511d5182ebac167e0b066d1bf67762
Greg Hamel <hamegreg@gmail.com>
2019-01-24 13:15:43 -0500

    refactor(header): remove logs

--- client/src/components/header.react.js
+++ client/src/components/header.react.js
@@ -4,8 +4,6 @@ import logo from './logo.svg';
 
 class Header extends Component {
   render() {
-    console.log(this.props)
-    console.log(typeof this.props.searchSubmit)
     return (
       <header className="app-header">
         <div className="app-header-content">
